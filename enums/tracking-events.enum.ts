ef5afecc9f972ace97f5bcfc6da20fc710ce4ecd
danielVFS <daniel.vitor@cuia.com.br>
2022-04-16 09:23:08 -0300

    feat(mixpanel): mixpanel singleton service

---
+++ enums/tracking-events.enum.ts
@@ -0,0 +1,6 @@
+enum TrackingEvents {
+  PAGE_VIEWED = "page_viewed",
+  PRODUCT_CLICKED = "product_clicked",
+}
+
+export default TrackingEvents;
---
+++ services/mixpanel.ts
@@ -0,0 +1,44 @@
+import mixpanel from "mixpanel-browser";
+import TrackingEvents from "../enums/tracking-events.enum";
+import { Product } from "../pages/products";
+
+export class MixpanelTracking {
+  private static _instance: MixpanelTracking;
+
+  public static getInstance(): MixpanelTracking {
+    if (MixpanelTracking._instance == null) {
+      return (MixpanelTracking._instance = new MixpanelTracking());
+    }
+
+    return this._instance;
+  }
+
+  public constructor() {
+    if (MixpanelTracking._instance)
+      throw new Error(
+        "Error: Instance creation of MixpanelTracking not alowed. Use Mixpanel.getInstance() instead."
+      );
+
+    mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_ID || "", {
+      debug: true,
+      ignore_dnt: true,
+    });
+  }
+
+  protected track(name: string, data: object = {}): void {
+    mixpanel.track(name, data);
+  }
+
+  public pageViewed(): void {
+    this.track(TrackingEvents.PAGE_VIEWED);
+  }
+
+  public productClicked(product: Product): void {
+    this.track(TrackingEvents.PRODUCT_CLICKED, {
+      productId: product.id,
+      productName: product.name,
+      productPrice: product.price,
+      productDescription: product.description,
+    });
+  }
+}
